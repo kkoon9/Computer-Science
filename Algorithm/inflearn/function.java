ef933a395df03d3ca6dea4d7bcd1e2f39128d1d6
kkoon9 <rndrnjs2003@gmail.com>
2020-08-30 13:25:36 +0900

    [Algorithm] 구현 문제 - 진약수의 합
    - 자연수 N이 입력되면 N의 진약수들의 합을 수식과 함께 출력하는 프로그램

--- Algorithm/inflearn/function.java
+++ Algorithm/inflearn/function.java
@@ -33,3 +33,26 @@ class SumofNaturalNumber {
     return answer;
   }
 }
+
+/**
+ * @desc 자연수 N이 입력되면 N의 진약수들의 합을 수식과 함께 출력하는 프로그램
+ * @param 3<=N<=100
+ * @return 수식과 함께 합을 출력(answer)
+ */
+class ProperDivisor {
+  public String solution(int N) {
+    String answer = "";
+    int sum = 0;
+    for (int i = 1; i < N; i++) {
+      if (N % i == 0) {
+        sum += i;
+        answer += String.valueOf(i);
+        answer += " + ";
+      }
+    }
+    answer = answer.substring(0, answer.length() - 3);
+    answer += " = ";
+    answer += String.valueOf(sum);
+    return answer;
+  }
+}
