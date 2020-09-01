15a17cac7de88f38eb127010c3eab7dd80179359
kkoon9 <rndrnjs2003@gmail.com>
2020-09-01 14:14:25 +0900

    [Algorithm] 정렬 문제 - 3등의 성적은?
    -  3등의 성적을 구하는 프로그램

--- Algorithm/inflearn/sort.java
+++ Algorithm/inflearn/sort.java
@@ -17,3 +17,43 @@ class SelectionSort {
     return arr;
   }
 }
+
+/**
+ * @desc 3등의 성적을 구하는 프로그램
+ * @param 정수(N), N의 길이를 가지는 배열(arr)
+ * @return 3등의 성적
+ */
+class GetThirdScore {
+  public int solution(int N, int[] arr) {
+    int answer = 0;
+    arr = selectionSort(N, arr);
+    for (int num : arr) {
+      System.out.print(num + " ");
+    }
+    System.out.println();
+    int rank = 1;
+    for (int i = 1; i < N; i++) {
+      if (arr[i - 1] != arr[i]) {
+        rank++;
+      }
+      if (rank == 3) {
+        answer = arr[i];
+        break;
+      }
+    }
+    return answer;
+  }
+
+  int[] selectionSort(int N, int[] arr) {
+    for (int i = 0; i < N; i++) {
+      int idx = i; // 가장 작은 값의 index
+      for (int j = i + 1; j < N; j++)
+        if (arr[idx] < arr[j])
+          idx = j;
+      int temp = arr[idx];
+      arr[idx] = arr[i];
+      arr[i] = temp;
+    }
+    return arr;
+  }
+}
