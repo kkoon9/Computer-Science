8188b6c0514c5a9ff4d8f15539aeaedd8a8ad1af
kkoon9 <rndrnjs2003@gmail.com>
2020-09-01 14:39:16 +0900

    [Algorithm] 정렬 문제 - 스페셜 정렬(사용자 정의 정렬)
    - 부호가 다르다면 오름차순
    - 부호가 같다면 절대값을 기준으로 오름차순

--- Algorithm/inflearn/sort.java
+++ Algorithm/inflearn/sort.java
@@ -76,3 +76,42 @@ class BubbleSort {
     return arr;
   }
 }
+
+/**
+ * @desc 부호가 다르다면 오름차순
+ * @desc 부호가 같다면 절대값을 기준으로 오름차순
+ * @param 정수(N), N의 길이를 가지는 배열(arr)
+ * @return 정렬된 배열(arr)
+ */
+class SpecialSort {
+  class SpecialArray implements Comparable<SpecialArray> {
+    int num;
+    int isPositive;
+
+    SpecialArray(int num, int isPositive) {
+      this.num = num;
+      this.isPositive = isPositive;
+    }
+
+    @Override
+    public int compareTo(SpecialArray o) {
+      int result = this.isPositive - o.isPositive;
+      if (result == 0) {
+        result = Math.abs(this.num) - Math.abs(o.num);
+      }
+      return result;
+    }
+  }
+
+  public int[] solution(int N, int[] arr) {
+    SpecialArray[] sa = new SpecialArray[arr.length];
+    for (int i = 0; i < N; i++) {
+      sa[i] = new SpecialArray(arr[i], arr[i] > 0 ? 1 : 0);
+    }
+    Arrays.sort(sa);
+    for (int i = 0; i < N; i++) {
+      arr[i] = sa[i].num;
+    }
+    return arr;
+  }
+}
