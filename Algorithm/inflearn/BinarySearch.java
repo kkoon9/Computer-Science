6891f9a3e04a0148e9641aa61b3d5022b51bae8b
kkoon9 <rndrnjs2003@gmail.com>
2020-09-04 19:28:58 +0900

    [Algorithm] 탐색 문제 - 이진 탐색
    - M개의 DVD에 모든 동영상을 녹화하기 위한 DVD의 최소 용량 크기를 구하는 프로그램

--- Algorithm/inflearn/BinarySearch.java
+++ Algorithm/inflearn/BinarySearch.java
@@ -26,3 +26,49 @@ class BinarySearch {
     return answer;
   }
 }
+
+/**
+ * @desc M개의 DVD에 모든 동영상을 녹화하기 위한 DVD의 최소 용량 크기를 구하는 프로그램
+ * @param 정수(N), 길이가 N인 배열, DVD 개수
+ * @return DVD 최소 용량
+ */
+class Question43 {
+  public int solution(int N, int M, int[] arr) {
+    int answer = 0;
+    int left = 0;
+    int right = 0;
+    int mid;
+    int max = Integer.MIN_VALUE;
+    for (int i = 0; i < N; i++) {
+      right += arr[i];
+      if (max < arr[i]) {
+        max = arr[i];
+      }
+    }
+    mid = right / 2;
+    while (left <= right) {
+      // 가능하니까 더 줄여보자.
+      if (mid >= max && counting(arr, mid) <= M) {
+        right = mid - 1;
+        answer = mid;
+      } else {
+        left = mid + 1;
+      }
+      mid = (left + right) / 2;
+    }
+    return answer;
+  }
+
+  int counting(int[] arr, int mid) {
+    int sum = 0;
+    int cnt = 1;
+    for (int i = 0; i < arr.length; i++) {
+      if (sum + arr[i] > mid) {
+        cnt++;
+        sum = arr[i];
+      } else
+        sum = sum + arr[i];
+    }
+    return cnt;
+  }
+}
