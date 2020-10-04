079d33e9fef9745122697694544234d08178adf6
kkoon9 <rndrnjs2003@gmail.com>
2020-10-04 17:20:56 +0900

    [Algorithm] Deque 문제 - 카드2

---
+++ Algorithm/Deque/P2164.java
@@ -0,0 +1,29 @@
+
+/**
+ * @포인트 Deque
+ */
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    System.out.println(P2164());
+  }
+
+  static int N;
+
+  static int P2164() throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(br.readLine());
+    Deque<Integer> deque = new LinkedList<>();
+    for (int i = 1; i <= N; i++) {
+      deque.add(i);
+    }
+    while (deque.size() != 1) {
+      deque.pollFirst();
+      deque.addLast(deque.pollFirst());
+    }
+    return deque.poll();
+  }
+}
