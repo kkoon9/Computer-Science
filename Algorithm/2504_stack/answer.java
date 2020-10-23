29e4221b96ba98a4c13b362f060ccbd7d58f33a7
kkoon9 <rndrnjs2003@gmail.com>
2020-10-23 15:21:40 +0900

    [Algorithm] 스택 문제 - 괄호의 값 : 리팩토링 완료
    - 스택 하나만 사용하고 sum 변수를 사용하여 풀이하였다.

--- Algorithm/2504_stack/answer.java
+++ Algorithm/2504_stack/answer.java
@@ -10,81 +10,46 @@ public class Main {
 }
 
 class Baekjoon {
-  Stack<Character> stackBracket = new Stack<>();
-  Stack<Integer> stackNumber = new Stack<>();
+  Stack<Character> stack = new Stack<>();
 
   public int P2504() throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String S = br.readLine();
     int answer = 0;
+    int sum = 1;
     int stringSize = S.length();
     for (int i = 0; i < stringSize; i++) {
       char ch = S.charAt(i);
-      if (ch == ')') {
-        int num = 0;
-        while (!stackBracket.isEmpty()) {
-          char stackBracketPop = stackBracket.pop();
-          if (stackBracketPop == 'N') {
-            int stackNumberPop = stackNumber.pop();
-            if (num == 0) {
-              num = stackNumberPop;
-            } else {
-              num += stackNumberPop;
-            }
-          } else if (stackBracketPop == '(') {
-            num *= 2;
-            num = num == 0 ? 2 : num;
-            stackNumber.push(num);
-            stackBracket.push('N');
-            break;
-          } else {
+      switch (ch) {
+        case '(':
+          sum *= 2;
+          stack.push(ch);
+          break;
+        case '[':
+          sum *= 3;
+          stack.push(ch);
+          break;
+        case ')':
+          if (stack.isEmpty() || stack.pop() != '(') {
             return 0;
           }
-        }
-        if (num == 0) {
-          return 0;
-        }
-      } else if (ch == ']') {
-        int num = 0;
-        while (!stackBracket.isEmpty()) {
-          char stackBracketPop = stackBracket.pop();
-          if (stackBracketPop == 'N') {
-            int stackNumberPop = stackNumber.pop();
-            if (num == 0) {
-              num = stackNumberPop;
-            } else {
-              num += stackNumberPop;
-            }
-          } else if (stackBracketPop == '[') {
-            num *= 3;
-            num = num == 0 ? 3 : num;
-            stackNumber.push(num);
-            stackBracket.push('N');
-            break;
-          } else {
+          if (S.charAt(i - 1) == '(')
+            answer += sum;
+          sum /= 2;
+          break;
+        case ']':
+          if (stack.isEmpty() || stack.pop() != '[') {
             return 0;
           }
-        }
-        if (num == 0) {
-          return 0;
-        }
-      } else {
-        stackBracket.push(ch);
-      }
-      /*
-       * for(char cha : stackBracket){ System.out.print(cha + " "); }
-       * System.out.print(" num : "); System.out.println();
-       */
-    }
-    for (char ch : stackBracket) {
-      if (ch != 'N') {
-        return 0;
+          if (S.charAt(i - 1) == '[')
+            answer += sum;
+          sum /= 3;
+          break;
       }
     }
-    for (int num : stackNumber) {
-      answer += num;
+    if (!stack.isEmpty()) {
+      return 0;
     }
-
     return answer;
   }
 }
